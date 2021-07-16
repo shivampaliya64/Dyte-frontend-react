45e9a2f2c2bc9a2e607e1c5352aa7e80c8dd6eb1
shivampaliya64 <42061891+shivampaliya64@users.noreply.github.com>
2021-07-17 00:29:12 +0530

    TS based, basics functional, updates needed

---
+++ editor_with_react/src/components/App.tsx
@@ -0,0 +1,63 @@
+import { time } from "console";
+import React, {useState, useEffect} from "react";
+import Editor from "./Editor";
+
+
+function App(){
+    const [html,setHtml]=useState('') 
+    const [css,setCss]=useState('') 
+    const [js,setJs]=useState('') 
+    const [srcDoc,setSrcDoc] = useState('')
+
+    useEffect(()=>{
+        const timeout = setTimeout(()=>{
+            setSrcDoc(`
+            <html>
+                <body>${html}</body>
+                <style>${css}</style>            
+            </html>
+        `)
+        },250)
+        return () => clearTimeout(timeout)//waits for 250 ms and then renders the component
+    },[html,css,js])
+
+    //const srcDoc  = 
+
+    return (
+        <>
+            <div className="pane top-pane">
+            <Editor 
+             language="xml"
+             displayName="HTML"
+             value={html}
+             onChange={setHtml}
+            />
+            <Editor 
+             language="css"
+             displayName="CSS"
+             value={css}
+             onChange={setCss}
+            />
+            <Editor 
+             language="javascript"
+             displayName="JS"
+             value={js}
+             onChange={setJs}
+            />
+            </div>
+            <div className="pane">
+                <iframe 
+                    srcDoc={srcDoc}
+                    title="output"
+                    sandbox="allow-scripts"
+                    frameBorder="0"
+                    width="100%"
+                    height="100%"
+                />
+            </div>
+        </>
+    )
+}
+export default App;
+
+
